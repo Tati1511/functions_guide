638605ab779545b4d78deed61e53b19ca78121d1
Tati1511 <102051224+Tati1511@users.noreply.github.com>
2022-04-04 23:02:00 -0400

    functions

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+fn counting (count: i32) {
+    println!("I am counting to 10. Currently at  {}", count);
+  
+}
+
+fn main() {
+    counting(1);
+    counting (2);
+    counting (3);
+    println!("I am bored now!")
+}
